23c689b1fc6ddaca22b2f81efbd36648c0671ef2
1411222022 <tsoyu5642@gmail.com>
2026-01-14 11:51:53 +0800

    0114

---
+++ Public/js/main.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const path = require("path");
+const DB = require("nedb-promises");
+
+const app = express();
+const PORT = 8090;
+
+// DB file
+const contactDb = DB.create(path.join(__dirname, "Contact.db"));
+
+// static files: /Public
+app.use(express.static(path.join(__dirname, "Public")));
+
+// body parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// home
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "Public", "indexx.html"));
+});
+
+// save contact (Vue fetch -> /contact)
+app.post("/contact", async (req, res) => {
+  try {
+    const data = {
+      email: req.body.email || "",
+      phone: req.body.phone || "",
+      name: req.body.name || "",
+      message: req.body.message || "",
+      createdAt: new Date().toISOString()
+    };
+
+    await contactDb.insert(data);
+    res.json({ ok: true, message: "Saved!", data });
+  } catch (err) {
+    res.status(500).json({ ok: false, message: "Save failed", error: String(err) });
+  }
+});
+
+// list contacts (check)
+app.get("/contacts", async (req, res) => {
+  try {
+    const list = await contactDb.find({}).sort({ createdAt: -1 });
+    res.json(list);
+  } catch (err) {
+    res.status(500).json({ ok: false, message: "Query failed", error: String(err) });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://127.0.0.1:${PORT}/`);
+});
--- server.js
+++ server.js
@@ -1,86 +1,53 @@
-var express = require("express");
-var server = express();
-var bodyParser = require("body-parser");
+const express = require("express");
+const path = require("path");
+const DB = require("nedb-promises");
 
+const app = express();
+const PORT = 8090;
 
-server.set("view engine", "ejs");
-server.set("views", __dirname + "/views");
+// DB file
+const contactDb = DB.create(path.join(__dirname, "Contact.db"));
 
-var fileUpload = require("express-fileupload");
+// static files: /Public
+app.use(express.static(path.join(__dirname, "Public")));
 
-server.use(express.static(__dirname + "/Public"));
-server.use(bodyParser.urlencoded());
-server.use(bodyParser.json());
-server.use(fileUpload({limits:{fileSize:2*1024*1024}}))
+// body parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-var DB=require("nedb-promises");
-var ServiceDB = DB.create(__dirname+"/Service.db");
-var PorfolioDB = DB.create(__dirname+"/Porfolio.db");
-var ContactDB = DB.create(__dirname+"/Contact.db");
-// ServiceDB.insert([
-//         { icon: 'fa-shopping-cart', title: 'E-Commerce', text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur porro laborum fuga repellat necessitatibus corporis nulla, in ex velit recusandae obcaecati maiores, doloremque quisquam similique, tempora aspernatur eligendi delectus! Rem.' },
-//         { icon: 'fa-laptop', title: 'Responsive Design', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.' },
-//         { icon: 'fa-lock', title: 'Web Security', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.' }
-//     ])
-//  PorfolioDB.insert( [
-//         { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
-//         { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
-//         { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" },
-//         { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
-//         { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
-//         { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" }
-//     ])
+// home
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "Public", "indexx.html"));
+});
 
-server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/Public/index.html");
-})
-server.get("/services", (req, res) => {
-    //db 
-    // var Services = [
-    //     { icon: 'fa-shopping-cart', title: 'E-Commerce', text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur porro laborum fuga repellat necessitatibus corporis nulla, in ex velit recusandae obcaecati maiores, doloremque quisquam similique, tempora aspernatur eligendi delectus! Rem.' },
-    //     { icon: 'fa-laptop', title: 'Responsive Design', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.' },
-    //     { icon: 'fa-lock', title: 'Web Security', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.' }
-    // ]
-    ServiceDB.find({},{_id:0}).then(results=>{
-       
-        res.send(results);
-    }).catch(error=>{
+// save contact (Vue fetch -> /contact)
+app.post("/contact", async (req, res) => {
+  try {
+    const data = {
+      email: req.body.email || "",
+      phone: req.body.phone || "",
+      name: req.body.name || "",
+      message: req.body.message || "",
+      createdAt: new Date().toISOString()
+    };
 
-    })
-    
-})
+    await contactDb.insert(data);
+    res.json({ ok: true, message: "Saved!", data });
+  } catch (err) {
+    res.status(500).json({ ok: false, message: "Save failed", error: String(err) });
+  }
+});
 
-server.get("/portfolio", (req, res) => {
-    // var Portfolio = [
-    //     { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
-    //     { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
-    //     { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" },
-    //     { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
-    //     { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
-    //     { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" }
-    // ]
-    PorfolioDB.find({}).then(results=>{
-        res.send(results);
-    })
-    
-})
+// list contacts (check)
+app.get("/contacts", async (req, res) => {
+  try {
+    const list = await contactDb.find({}).sort({ createdAt: -1 });
+    res.json(list);
+  } catch (err) {
+    res.status(500).json({ ok: false, message: "Query failed", error: String(err) });
+  }
+});
 
-server.get("/about", (req, res) => {
-    res.send("Welcome " + req.query.user + " to My first NodeJS server!");
-})
-
-server.post("/contact", (req, res) =>{
-    ContactDB.insert(req.body);
-    //move to public/upload
-    var upFile=req.files.myFile1;
-    upFile.mv(__dirname+"/Public/upload/"+upFile.name, function(err){
-        if(err==null){
-            res.render("msg",{message:"I got a file: "+upFile.name})
-        }else{
-            res.render("msg",{message:err});
-        }
-    })
-})
-
-
-server.listen(8090)
+app.listen(PORT, () => {
+  console.log(`Server running at http://127.0.0.1:${PORT}/`);
+});
